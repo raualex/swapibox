f0339d7c1b7d204eff8041be5c840807e2a18b6d
Graham <g@masterofnone.com>
2018-10-08 17:10:03 -0600

    Add base structure to start iteration one

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import Nav from '../Nav';
+import CardContainer from '../CardContainer';
+
+import './App.css';
+
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default App;
