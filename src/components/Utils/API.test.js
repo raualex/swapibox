3384499addb0548891eddec2c6c34ee94087cf62
Graham <g@masterofnone.com>
2018-10-13 19:02:37 -0600

    Add tests for local storage

--- src/components/Utils/API.test.js
+++ src/components/Utils/API.test.js
@@ -33,8 +33,78 @@ describe('API', () => {
     expect(result).toEqual(false)
   });
 
-  it('should return array of correct card types', async () => {    
-    
+  it('should return array of correct card types', async () => {   
+    const cards = [{ name: 'harry', type: 'people' },
+    { name: 'rover', type: 'vehicles' },
+    { name: 'zonie', type: 'planets' }] 
+    localStorage.setItem('cards', JSON.stringify(cards))
+
+    const expected = [cards[1]];
+    const result = APP.checkLocalStorage('vehicles')
+
+    expect(result).toEqual(expected)
   });
+
+  it('should return array of cleaned films', async () => { 
+    const cards = [films.results[0]]
+    const expected = [{
+      title: films.results[0].title,
+      opening_crawl: films.results[0].opening_crawl,
+      type: 'films'
+    }]
+
+    const result = await APP.getFilms(cards)
+    expect(result).toEqual(expected)
+  });
+
+  it('find and change the favorite boolean to update favorites', async () => {   
+    const cards = [{ name: 'harry', type: 'people', favorite: false },
+    { name: 'rover', type: 'vehicles', favorite: false },
+    { name: 'zonie', type: 'planets', favorite: false }] 
+    localStorage.setItem('cards', JSON.stringify(cards))
+
+    const expected = [{ name: 'harry', type: 'people', favorite: true },
+    { name: 'rover', type: 'vehicles', favorite: false },
+    { name: 'zonie', type: 'planets', favorite: false }] 
+    
+    APP.updateFavorites('harry')
+
+    expect(JSON.parse(localStorage.getItem('cards'))).toEqual(expected)
+  });
+
+  it('should set local storage', async () => {   
+    const cards = [{ name: 'harry', type: 'people', favorite: false },
+    { name: 'zonie', type: 'planets', favorite: false }] 
+
+    const expected = [{ name: 'harry', type: 'people', favorite: false },
+    { name: 'zonie', type: 'planets', favorite: false },
+    { name: 'rover', type: 'vehicles', favorite: false }]
+    localStorage.setItem('cards', JSON.stringify(cards))
+
+
+    const data = [{ name: 'rover', type: 'vehicles', favorite: false }]
+    APP.setLocalStorage(data, 'vehicles')
+
+    expect(JSON.parse(localStorage.getItem('cards'))).toEqual(expected)
+  });
+
+
+  it('get people from fetch call', async () => {   
+    const cards = [{ name: 'harry', type: 'people', favorite: false },
+    { name: 'zonie', type: 'planets', favorite: false }] 
+
+    const expected = [{ name: 'harry', type: 'people', favorite: false },
+    { name: 'zonie', type: 'planets', favorite: false },
+    { name: 'rover', type: 'vehicles', favorite: false }]
+    localStorage.setItem('cards', JSON.stringify(cards))
+
+
+    const data = [{ name: 'rover', type: 'vehicles', favorite: false }]
+    APP.setLocalStorage(data, 'vehicles')
+
+    expect(JSON.parse(localStorage.getItem('cards'))).toEqual(expected)
+  });
+
+
 
 })
