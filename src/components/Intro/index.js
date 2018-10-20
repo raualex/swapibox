ec18e69f55bd634bbc47e52a9c8f6cbba2b2899a
Graham <g@masterofnone.com>
2018-10-20 15:19:26 -0600

    Mitigate crawl error - determine double render issue

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -3,20 +3,25 @@ import './Intro.css';
 import PropTypes from 'prop-types';
 
 const Intro = ({ films }) => {
-  const randomNumber = Math.round(Math.random() * films.length);
-
+  const randomNumber = Math.round(Math.random() * films.length) + 1;
+  console.log(films[randomNumber])
   return (
     <div>
-    <div className="fade"></div>
-      <section className="star-wars">
-        <div className="crawl">
-          <div className="title">
-            <h1>{films[randomNumber].title}</h1>
-            <p>{films[randomNumber].release_date}</p> 
-          </div>
-          <p>{films[randomNumber].opening_crawl}</p>  
-        </div>
-      </section>
+    {
+      films[randomNumber] !== undefined &&
+      <div>
+        <div className="fade"></div>
+          <section className="star-wars">
+            <div className="crawl">
+              <div className="title">
+                <h1>{films[randomNumber].title}</h1>
+                <p>{films[randomNumber].release_date}</p> 
+              </div>
+              <p>{films[randomNumber].opening_crawl}</p>  
+            </div>
+          </section>
+      </div>
+      }
     </div>
   )
 }
