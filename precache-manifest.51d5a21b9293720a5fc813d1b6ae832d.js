750e1ce702f8fffe33d802e3270fe3482f2ab190
Graham <g@masterofnone.com>
2018-10-21 15:12:34 -0600

    Updates

---
+++ precache-manifest.51d5a21b9293720a5fc813d1b6ae832d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "37973e60331000f12a63c19569991587",
+    "url": "/swapibox/static/media/dagobah.37973e60.jpg"
+  },
+  {
+    "revision": "8f9bea91f509392f99af",
+    "url": "/swapibox/static/css/main.02061522.chunk.css"
+  },
+  {
+    "revision": "94ea2d2dfc3b402db90a",
+    "url": "/swapibox/static/js/1.94ea2d2d.chunk.js"
+  },
+  {
+    "revision": "c9f58f50f745c5d4961b",
+    "url": "/swapibox/static/js/runtime~main.c9f58f50.js"
+  },
+  {
+    "revision": "32bbee51d3f9550071d2673f9e11253b",
+    "url": "/swapibox/static/media/logo.32bbee51.svg"
+  },
+  {
+    "revision": "67fe0b4588067cc754ae98100f476693",
+    "url": "/swapibox/static/media/loading.67fe0b45.svg"
+  },
+  {
+    "revision": "2351d18314b8d549e2e2ee6c38d85578",
+    "url": "/swapibox/static/media/intro.2351d183.mp3"
+  },
+  {
+    "revision": "e6818dc90cce3cd8b4190916f36ad2bb",
+    "url": "/swapibox/static/media/luke-skywalker.e6818dc9.jpg"
+  },
+  {
+    "revision": "89ab994ac14bb66220d0ed5acc6c8fcf",
+    "url": "/swapibox/static/media/c-3po.89ab994a.jpg"
+  },
+  {
+    "revision": "964622df32e69b5dd87668be77bb5bcd",
+    "url": "/swapibox/static/media/r2-d2.964622df.jpg"
+  },
+  {
+    "revision": "7e495aeaa4f43592029893bb5d624c23",
+    "url": "/swapibox/static/media/darthvader.7e495aea.jpg"
+  },
+  {
+    "revision": "91ed31c6dd5c736406f35b213c40fd1f",
+    "url": "/swapibox/static/media/Leia.91ed31c6.jpg"
+  },
+  {
+    "revision": "55871c6012d2f93c6d01d00fe51393e9",
+    "url": "/swapibox/static/media/uncle.55871c60.jpg"
+  },
+  {
+    "revision": "1f711a5b240438cabcf89113d38aec12",
+    "url": "/swapibox/static/media/beru.1f711a5b.jpg"
+  },
+  {
+    "revision": "f362b49a6ae2207caa7f7979daf878ec",
+    "url": "/swapibox/static/media/r5-d4.f362b49a.jpg"
+  },
+  {
+    "revision": "bf02e5b3f458692264e33fe95a0f2094",
+    "url": "/swapibox/static/media/biggs.bf02e5b3.jpg"
+  },
+  {
+    "revision": "14452f35e2ac7c0bd99938e734d080a0",
+    "url": "/swapibox/static/media/obi-wan.14452f35.jpg"
+  },
+  {
+    "revision": "1ada9bad9f874d2bd52121cb075da483",
+    "url": "/swapibox/static/media/alderaan.1ada9bad.jpg"
+  },
+  {
+    "revision": "79158cb4d2cbe1c24bfff2d418cc758b",
+    "url": "/swapibox/static/media/yavin.79158cb4.jpg"
+  },
+  {
+    "revision": "4b87d506716f594d9e053a75511efd71",
+    "url": "/swapibox/static/media/hoth.4b87d506.jpg"
+  },
+  {
+    "revision": "8f9bea91f509392f99af",
+    "url": "/swapibox/static/js/main.8f9bea91.chunk.js"
+  },
+  {
+    "revision": "c7062e4f011fa943253fbc34a2089115",
+    "url": "/swapibox/static/media/bespin.c7062e4f.jpg"
+  },
+  {
+    "revision": "6ef0385901f80ba0108c690444c74fb6",
+    "url": "/swapibox/static/media/endor.6ef03859.jpg"
+  },
+  {
+    "revision": "24c7b65023b8b67a2733ca059b856ff9",
+    "url": "/swapibox/static/media/noboo.24c7b650.jpg"
+  },
+  {
+    "revision": "0d30b9ff2b5532740ae789733e10f434",
+    "url": "/swapibox/static/media/cruscant.0d30b9ff.jpg"
+  },
+  {
+    "revision": "53e45565091ff26376a349ad102ae7c4",
+    "url": "/swapibox/static/media/kamino.53e45565.jpg"
+  },
+  {
+    "revision": "21d8e596630de43eb1348f59111742ad",
+    "url": "/swapibox/static/media/geonosis.21d8e596.jpg"
+  },
+  {
+    "revision": "a3fd37abee4b85465078fef0cef268f9",
+    "url": "/swapibox/static/media/sand.a3fd37ab.jpg"
+  },
+  {
+    "revision": "6411bbf363ac5a687354af04be05a407",
+    "url": "/swapibox/static/media/skyhopper.6411bbf3.jpg"
+  },
+  {
+    "revision": "fe024e8b89d6e814805c82835a434a86",
+    "url": "/swapibox/static/media/landspeeder.fe024e8b.jpg"
+  },
+  {
+    "revision": "91d99af2a2f6d4d1adf3d4f0be1626c7",
+    "url": "/swapibox/static/media/tie.91d99af2.jpg"
+  },
+  {
+    "revision": "927a9a30974c03d7976693c0a9185695",
+    "url": "/swapibox/static/media/snowspeed.927a9a30.jpg"
+  },
+  {
+    "revision": "bf5ba4ff48b4a8b285433fae10f4e69a",
+    "url": "/swapibox/static/media/bomber.bf5ba4ff.jpg"
+  },
+  {
+    "revision": "cc2f2108ae4cac6f4b00407e86ec3bbf",
+    "url": "/swapibox/static/media/at-at.cc2f2108.jpg"
+  },
+  {
+    "revision": "a74aa15f7de7d2fa664f68d82d7fea2b",
+    "url": "/swapibox/static/media/at-st.a74aa15f.jpg"
+  },
+  {
+    "revision": "48f35a904e6f4f12e9d03efe9b87e749",
+    "url": "/swapibox/static/media/pod.48f35a90.jpg"
+  },
+  {
+    "revision": "838c288e490139dd25536bd7f2b84054",
+    "url": "/swapibox/static/media/sail.838c288e.jpg"
+  },
+  {
+    "revision": "3dd8963f4bff1eff43ac951feda3a54d",
+    "url": "/swapibox/static/media/star-true.3dd8963f.svg"
+  },
+  {
+    "revision": "7991d3aacc4c23293aa1609990550280",
+    "url": "/swapibox/static/media/star-false.7991d3aa.svg"
+  },
+  {
+    "revision": "1ee9abc54c8ffcc1efaaee114cad86fd",
+    "url": "/swapibox/static/media/stars-in-space-moving-fast-background_v1njcikee__F0000.1ee9abc5.png"
+  },
+  {
+    "revision": "ec406ef10754ca23a8be27a1d3163106",
+    "url": "/swapibox/index.html"
+  }
+];
